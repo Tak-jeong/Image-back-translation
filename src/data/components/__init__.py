a4039bb0052436796894b011088280cb0c281b32
Tak-jeong <jungtok@gmail.com>
2024-11-12 17:47:34 +0900

    capmix update #2

--- src/data/components/__init__.py
+++ src/data/components/__init__.py
@@ -1,2 +1,4 @@
 from .mixup_cutmix import CutMixUp
-from .MixAug import MixAugDataset
+from .MixAug import MixAugDataset, MixCutDataset
+from .ImageNet import ImageNetDataset, SDDataset
+from .CapMix import CapMixDataset
